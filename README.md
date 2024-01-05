84c9a1280f72c6ff4d15f7790112a411c5256f47
Jonas <turtelmaster11@gmail.com>
2024-01-05 01:50:21 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # snooptiTools
+Multitool with some nice features
+
+# Changelog
+----- v1.0.7
+- optimization
+- name changed
+- changed updatelink
+
+----- v1.0.6
+- sourcecode link added
+
+---- v1.0.5
+- Changed version updater method
+
+----- v1.0.4
+- changelog
+
+----- v1.0.3
+- bug fixes
+
+----- v1.0.2
+- design update
+
+----- v1.0.1
+- Winget updater
+- Calculator
+- Download latest version
+
+----- v1.0.0
+- Beta Release
--- snooTools.bat
+++ snooTools.bat
@@ -24,6 +24,7 @@ echo ---------- %name% %version% ----------
 echo.
 echo [1] Winget Updater
 echo [2] Calculator
+echo [3] File Downloader
 echo.
 echo [#] Download latest version
 echo [?] Sourcecode (github)
@@ -34,6 +35,7 @@ set /p choice=Insert option:
 
 if "%choice%"=="1" goto tool-updater
 if "%choice%"=="2" goto tool-calculator
+if "%choice%"=="3" goto file-downloader
 if "%choice%"=="#" goto updateversion
 if "%choice%"=="?" goto sourcecode
 if "%choice%"=="-" goto changelog
@@ -44,6 +46,28 @@ echo Invalid choice. Please select a valid option.
 timeout /t 2 >nul
 goto menu
 
+::---------------------------------- GENERAL ----------------------------------
+::----------------- VersionUpdate
+:updateversion
+cls
+echo.
+echo Downloading from %updateURL%
+echo.
+timeout /t 2 >nul
+curl -o temp.bat %updateURL%
+timeout /t 1 >nul
+move /y temp.bat "%~0"
+timeout /t 3 >nul
+exit /b
+
+::----------------- Sourcecode
+:sourcecode
+cls
+start %sourceCode%
+goto menu
+
+
+::---------------------------------- TOOLS ----------------------------------
 ::----------------- WingetUpdater
 :tool-updater
 cls
@@ -77,58 +101,21 @@ echo Result: %result%
 pause >nul
 goto op-calculator
 
-::----------------- VersionUpdate
-:updateversion
+
+::----------------- File Downloader
+:file-downloader
 cls
 echo.
-echo Downloading from %updateURL%
+echo ---------- FILE DOWNLOADER ----------
 echo.
+set /p "fileURL=Enter file URL to download: "
+set /p "destination=Enter destination path to save the file: "
+
+echo.
+echo Downloading file from %fileURL%...
 timeout /t 2 >nul
-curl -o temp.bat %updateURL%
+curl -o "%destination%" %fileURL%
 timeout /t 1 >nul
-move /y temp.bat "%~0"
+echo Download complete.
 timeout /t 3 >nul
-exit /b
-
-::----------------- Sourcecode
-:sourcecode
-cls
-start %sourceCode%
-goto menu
-
-::----------------- Changelog
-:changelog 
-cls
-echo.
-echo ---------- CHANGELOG ----------
-echo.
-echo ----- v1.0.7
-echo + optimization
-echo + name changed
-echo + changed updatelink
-echo.
-echo ----- v1.0.6
-echo + sourcecode link added
-echo.
-echo ----- v1.0.5
-echo changed version updater method
-echo.
-echo ----- v1.0.4
-echo + changelog
-echo.
-echo ----- v1.0.3
-echo bug fixes
-echo.
-echo ----- v1.0.2
-echo design update
-echo.
-echo ----- v1.0.1
-echo + Winget updater
-echo + Calculator
-echo + Download latest version
-echo.
-echo ----- v1.0.0
-echo Beta Release
-echo.
-pause
 goto menu
