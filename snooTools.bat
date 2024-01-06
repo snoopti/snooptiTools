f0fe1bb5060a007bc8abf2f606b30236a07a960d
Jonas <turtelmaster11@gmail.com>
2024-01-06 20:57:48 +0100

    cleaner

--- snooTools.bat
+++ snooTools.bat
@@ -23,7 +23,7 @@ echo.
 echo ---------- %name% %version% ----------
 echo.
 echo [1] Winget Updater
-echo [2] Calculator
+echo [2] Cleaner
 echo.
 echo [#] Update Version
 echo [?] Sourcecode
@@ -32,7 +32,7 @@ echo.
 set /p choice=Insert option: 
 
 if "%choice%"=="1" goto tool-updater
-if "%choice%"=="2" goto tool-calculator
+if "%choice%"=="2" goto tool-cleaner
 
 if "%choice%"=="#" goto updateversion
 if "%choice%"=="?" goto sourcecode
@@ -84,16 +84,50 @@ echo Update process complete.
 timeout /t 10 >nul
 goto menu
 
-::----------------- Calculator
-:tool-calculator
+::----------------- Cleaner
+setlocal enabledelayedexpansion
+
+:cleanFolder
+if exist "%~1" (
+    rmdir /s /q "%~1" 2>nul
+    if errorlevel 1 (
+        echo Failed to clean %~1. Possibly in use.
+    ) else (
+        echo Successfully cleaned %~1
+    )
+) else (
+    echo Folder not found, skipping cleanup
+)
+timeout /t 3 >nul
+exit /b
+
+:tool-cleaner
 cls
 echo.
-echo ---------- CALCULATOR ----------
+echo ---------- CLEANER ----------
 echo.
-echo Insert calculation:
-set /p "expression="
+echo You can ignore any errors like access denied or something; it's normal when some files are currently used by some programs.
 echo.
-for /f "delims=" %%a in ('powershell -command "write-host (%expression%)"') do set "result=%%a"
-echo Result: %result%
-pause >nul
-goto op-calculator
+echo Sit back and wait for it to finish.
+timeout /t 5 >nul
+cls
+
+echo [1] Cleaning Temporary Files
+call :cleanFolder "C:\Users\%USERNAME%\AppData\Local\Temp"
+
+echo [2] Cleaning Temporary Files
+call :cleanFolder "C:\Windows\Temp"
+
+echo [3] Cleaning Chrome Service Worker Cache
+call :cleanFolder "C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Service Worker\CacheStorage"
+
+echo [4] Cleaning Nvidia DXCache
+call :cleanFolder "C:\Users\%USERNAME%\AppData\Local\NVIDIA\DXCache"
+
+echo [5] Cleaning Old Windows Files
+call :cleanFolder "C:\windows.old"
+
+cls
+echo Cleaner finished.
+pause
+goto menu
